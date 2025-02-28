396e8d6a8207552892cdea03cdea598bda15ba83
ana-yet <anayethusenaj@gmail.com>
2025-03-01 03:50:59 +0600

    fixed

--- script/toggling.js
+++ script/toggling.js
@@ -1,5 +1,5 @@
 // blogs join
 document.getElementById("Blogs").addEventListener("click", function (event) {
   event.preventDefault();
-  window.location.href = "../qna.html";
+  window.location.href = "/assignment-5/qna.html";
 });
