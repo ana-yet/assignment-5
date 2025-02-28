4d9ddcc686eee92270d0efbf6d2e04fe01d68390
ana-yet <anayethusenaj@gmail.com>
2025-03-01 03:45:59 +0600

    fixed

--- script/toggling.js
+++ script/toggling.js
@@ -3,9 +3,3 @@ document.getElementById("Blogs").addEventListener("click", function (event) {
   event.preventDefault();
   window.location.href = "../qna.html";
 });
-
-// qn
-document.getElementById("qn-btn").addEventListener("click", function (event) {
-  event.preventDefault();
-  window.location.href = "../index.html";
-});
